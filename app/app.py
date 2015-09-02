25d0f752edc5509eaa4fbaac5f873c21796fcfdf
Jim Sims <jim.sims@isilon.com>
2015-09-02 10:08:19 -0700

    fixed app to answer on virtual face and changed route /

--- app/app.py
+++ app/app.py
@@ -2,8 +2,8 @@ from bottle import Bottle, run
 
 app = Bottle()
 
-@app.route('/hello')
+@app.route('/')
 def hello():
     return "Hello World!"
 
-run(app, host='localhost', port=80)
+run(app, host='0.0.0.0', port=80)
